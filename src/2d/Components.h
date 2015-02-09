6239c7eb8ee6e57e21eca7df28cbed16b04b12f7
David Wicks <wicks.db@gmail.com>
2015-02-09 13:22:34 -0500

    Starting to concretize things.
    Stubs for a 2d tree setup.

---
+++ src/2d/Components.h
@@ -0,0 +1,27 @@
+
+#include "treent/TreeComponent.h"
+
+namespace treent
+{
+
+struct TransformComponent : public TreeComponent<TransformComponent>
+{
+	void updateChild (TransformComponent &child) { child._position += _position; child._rotation += _rotation; }
+	void updateParent (TransformComponent &parent) {}
+	void compose (const TransformComponent &other) { _position += other._position; _rotation += other._rotation; }
+
+	ci::vec2	_position;
+	float			_rotation;
+};
+
+struct StyleComponent : public TreeComponent<StyleComponent>
+{
+	void updateChild(StyleComponent &child) { child._alpha *= _alpha; }
+	void updateParent(StyleComponent &parent) {}
+	void compose(const StyleComponent &other) { _alpha *= other._alpha; }
+
+	float 		_alpha;
+	ci::vec3	_color;
+};
+
+} // namespace treent
---
+++ src/2d/Treent2d.h
@@ -0,0 +1,11 @@
+
+#pragma once
+
+#include "Components.h"
+
+namespace treent
+{
+
+using Treent2D = Treent<TransformComponent, StyleComponent>;
+
+} // namespace treent
