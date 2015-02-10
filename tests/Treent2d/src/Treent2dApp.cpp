b56d6c6d9758b936fb493e9fb84bd97a5bb863e1
David Wicks <wicks.db@gmail.com>
2015-02-10 11:17:22 -0500

    Test project and cinder block file.

---
+++ tests/Treent2d/src/Treent2dApp.cpp
@@ -0,0 +1,48 @@
+#include "cinder/app/AppNative.h"
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+
+#include "entityx/Entity.h"
+#include "treent/2d/Treent2d.h"
+
+#include "cinder/svg/Svg.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class Treent2dApp : public AppNative {
+  public:
+	void setup() override;
+	void mouseDown( MouseEvent event ) override;
+	void update() override;
+	void draw() override;
+};
+
+void Treent2dApp::setup()
+{
+  entityx::EventManager events;
+  entityx::EntityManager entities(events);
+
+  auto ent = make_shared<treent::Treent2D>(entities);
+//  auto c = ent->createChild();
+//  ent->removeChild(c);
+
+  svg::Node;
+
+}
+
+void Treent2dApp::mouseDown( MouseEvent event )
+{
+}
+
+void Treent2dApp::update()
+{
+}
+
+void Treent2dApp::draw()
+{
+	gl::clear( Color( 0, 0, 0 ) ); 
+}
+
+CINDER_APP_NATIVE( Treent2dApp, RendererGl )
